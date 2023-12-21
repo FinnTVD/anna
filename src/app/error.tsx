8a9b498ef037dcbb9dd2d39144df66b5711903ef
nguyenminhhung <hungnm.17k2@gmail.com>
2023-12-21 09:00:59 +0700

    fix deploy

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -19,12 +19,7 @@ export default function Error({
   return (
     <div>
       <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
+      <button type="button" onClick={reset}>
         Try again
       </button>
     </div>
