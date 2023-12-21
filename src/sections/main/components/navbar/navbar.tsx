27667aae06452c44c51128defe55838b1d3e89cc
nguyenminhhung <hungnm.17k2@gmail.com>
2023-12-21 14:19:11 +0700

    fix deploy

--- src/sections/main/components/navbar/navbar.tsx
+++ src/sections/main/components/navbar/navbar.tsx
@@ -1,11 +1,8 @@
-import { NEXT_AUTH_OPTIONS } from '@/configs/auth-option';
 import { cn } from '@/lib/utils';
-import { getServerSession } from 'next-auth/next';
 import NavItems from './nav-items/nav-items';
 import NavMobileDetail from './nav-mobile-detail';
 
 const Navbar = async () => {
-  const session = await getServerSession(NEXT_AUTH_OPTIONS);
   return (
     <div>
       {/* <Logo /> */}
@@ -14,11 +11,11 @@ const Navbar = async () => {
           'flex justify-between w-full py-[6px] px-20 py-6 fixed top-[0px] z-10 bg-[white] max-md:hidden'
         )}
       >
-        <NavItems session={session} />
+        <NavItems />
         <div className="mt-5 max-md:mt-1" />
       </div>
       <div className="hidden max-sm:block fixed top-[20px] right-[3.2rem] z-10">
-        <NavMobileDetail session={session} />
+        <NavMobileDetail />
       </div>
     </div>
   );
