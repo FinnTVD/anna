bd172a6096f82f28f46731e0e3d03baf8a44f8c1
Hùng Nguyễn <hungnm.17k2@gmail.com>
2023-12-28 03:42:10 +0000

    Develop

--- src/sections/home/view/Blog/Item/index.tsx
+++ src/sections/home/view/Blog/Item/index.tsx
@@ -1,6 +1,7 @@
 import ICVectorRight from '@/components/Icons/ICVectorRight';
 import Image from 'next/image';
 import { formatDate } from '@/ultils/format-date';
+import serviceImg from '@/assets/blogImg/Rectangle719.png';
 
 interface IpropsBlog {
   dataBlog: any;
@@ -11,14 +12,14 @@ function ItemBlog({ dataBlog }: IpropsBlog) {
       <div className="pb-16 md:pb-4 bg-white w-full h-full rounded-[6.4rem] md:rounded-3xl border-2 border-gray-200 flex-col justify-between items-start gap-4 inline-flex">
         <div className="pb-6 w-full flex-col justify-start gap-5 flex">
           <Image
-            src="/img/home/image1836.png"
+            src={dataBlog?.thumbnail_url ? dataBlog?.thumbnail_url : serviceImg}
             width={445}
             height={304}
             alt=""
             className="w-full h-[55rem] md:h-[23.75rem] rounded-[6.4rem] md:rounded-3xl object-cover"
           />
           <div className="px-16 md:px-4 text-zinc-700 text-[6rem] md:text-2xl font-extrabold line-clamp-2">
-            {dataBlog?.title?.rendered}
+            {dataBlog?.title}
           </div>
         </div>
         <div className="self-stretch px-20 md:px-6 justify-between items-center inline-flex">
@@ -31,7 +32,7 @@ function ItemBlog({ dataBlog }: IpropsBlog) {
             </span>
             <span className="text-zinc-500 text-[4rem] md:text-lg font-bold">
               {' '}
-              Ngày {formatDate(dataBlog?.date)}
+              Ngày {formatDate(dataBlog?.post_date)}
             </span>
           </div>
           <div className="py-[15px] rounded-[50px] justify-center items-center gap-2.5 flex">
--- src/sections/main/components/navbarHome/nav-items/nav-items.tsx
+++ src/sections/main/components/navbarHome/nav-items/nav-items.tsx
@@ -13,6 +13,11 @@ import ICCart from '@/components/Icons/ICCart';
 import { Input } from '@/components/ui/input';
 import ICSearch from '@/components/Icons/ICSearch';
 import './style.css';
+import DropdownProductHeader from '@/components/component-ui-custom/dropdown-product-header';
+import DropdownSearchHeader from '@/components/component-ui-custom/dropdown-search-header';
+import DropdownSeeMoreHeader from '@/components/component-ui-custom/dropdown-see-more-header';
+import DropdownCartHeader from '@/components/component-ui-custom/dropdown-cart-header';
+import Link from 'next/link';
 
 interface IProps {
   styleNavbar?: boolean;
@@ -48,7 +53,11 @@ function NavItems({ styleNavbar }: IProps) {
         } w-full flex mt-2 px-7 py-[0.625rem] rounded-[6.25rem]`}
       >
         <div className="xl:w-[20px] xl:h-[20px] mr-[1rem]">
-          <ICLogo fill="#4DC0BD" width="2.8125rem" height="2.5rem" />
+          <ICLogo
+            fill={`${!styleNavbar ? '#fff' : '#4DC0BD'}`}
+            width="2.8125rem"
+            height="2.5rem"
+          />
         </div>
         <ul className="grow flex justify-between primary-nav md:flex text-[11px] font-bold items-center">
           <li className="active has-child mx-3.5">
@@ -72,47 +81,7 @@ function NavItems({ styleNavbar }: IProps) {
                   />
                 </a>
               </HoverCardTrigger>
-              <HoverCardContent
-                className="border-none p-0"
-                side="bottom"
-                align="start"
-              >
-                <ul>
-                  <li className="border-b px-2 py-[6px]">
-                    <a
-                      className="uppercase font-bold"
-                      href="src/sections/main-layout/components/navbar/nav-items"
-                    >
-                      Map Full Screen Sidebar Results
-                    </a>
-                  </li>
-                  <li className="border-b px-2 py-[6px]">
-                    <a
-                      className="uppercase font-bold"
-                      href="src/sections/main-layout/components/navbar/nav-items"
-                    >
-                      Map Full Screen Sidebar Results
-                    </a>
-                  </li>
-                  <li className="border-b px-2 py-[6px]">
-                    <a
-                      className="uppercase font-bold"
-                      href="src/sections/main-layout/components/navbar/nav-items"
-                    >
-                      Map Full Screen Sidebar Results
-                    </a>
-                  </li>
-                  <li className="border-b px-2 py-[6px]">
-                    <a
-                      className="uppercase font-bold"
-                      href="src/sections/main-layout/components/navbar/nav-items"
-                    >
-                      Map Full Screen Sidebar Results
-                    </a>
-                  </li>
-                </ul>
-                <HoverCardArrow className="fill-white shadow-md" />
-              </HoverCardContent>
+              <DropdownProductHeader />
             </HoverCard>
           </li>
           <li className="has-child flex items-center mx-3.5">
@@ -126,16 +95,23 @@ function NavItems({ styleNavbar }: IProps) {
             <ICLocation fill="#4DC0BD" width="1.25rem" height="1.51338rem" />
           </li>
           <li className="relative rounded-[6.25rem] grow has-child flex items-center grow mx-3.5">
-            <Input
-              className={`${
-                !styleNavbar ? 'search-home-navbar' : ''
-              } input-search rounded-[6.25rem] placeholder-[#4DC0BD] bg-[#EEFBFB] border-[#EEFBFB]`}
-              type="text"
-              placeholder="Tìm kiếm sản phẩm"
-            />
-            <div className="absolute top-[50%] -translate-y-1/2 right-[1rem]">
-              <ICSearch fill="#4DC0BD" width="1.00006rem" height="1rem" />
-            </div>
+            <HoverCard openDelay={0}>
+              <HoverCardTrigger asChild>
+                <div className="relative rounded-[6.25rem] flex items-center w-full">
+                  <Input
+                    className={`${
+                      !styleNavbar ? 'search-home-navbar' : ''
+                    } input-search rounded-[6.25rem] placeholder-[#4DC0BD] bg-[#EEFBFB] border-[#EEFBFB]`}
+                    type="text"
+                    placeholder="Tìm kiếm sản phẩm"
+                  />
+                  <div className="absolute top-[50%] -translate-y-1/2 right-[1rem]">
+                    <ICSearch fill="#4DC0BD" width="1.00006rem" height="1rem" />
+                  </div>
+                </div>
+              </HoverCardTrigger>
+              <DropdownSearchHeader />
+            </HoverCard>
           </li>
           <li className="active has-child mx-3.5">
             <HoverCard openDelay={0}>
@@ -158,47 +134,7 @@ function NavItems({ styleNavbar }: IProps) {
                   />
                 </a>
               </HoverCardTrigger>
-              <HoverCardContent
-                className="border-none p-0"
-                side="bottom"
-                align="start"
-              >
-                <ul>
-                  <li className="border-b px-2 py-[6px]">
-                    <a
-                      className="uppercase font-bold"
-                      href="src/sections/main-layout/components/navbar/nav-items"
-                    >
-                      Map Full Screen Sidebar Results
-                    </a>
-                  </li>
-                  <li className="border-b px-2 py-[6px]">
-                    <a
-                      className="uppercase font-bold"
-                      href="src/sections/main-layout/components/navbar/nav-items"
-                    >
-                      Map Full Screen Sidebar Results
-                    </a>
-                  </li>
-                  <li className="border-b px-2 py-[6px]">
-                    <a
-                      className="uppercase font-bold"
-                      href="src/sections/main-layout/components/navbar/nav-items"
-                    >
-                      Map Full Screen Sidebar Results
-                    </a>
-                  </li>
-                  <li className="border-b px-2 py-[6px]">
-                    <a
-                      className="uppercase font-bold"
-                      href="src/sections/main-layout/components/navbar/nav-items"
-                    >
-                      Map Full Screen Sidebar Results
-                    </a>
-                  </li>
-                </ul>
-                <HoverCardArrow className="fill-white shadow-md" />
-              </HoverCardContent>
+              <DropdownSeeMoreHeader />
             </HoverCard>
           </li>
           <li className="has-child flex items-center mx-3.5">
@@ -210,20 +146,31 @@ function NavItems({ styleNavbar }: IProps) {
               Hành trình tử tế
             </span>
           </li>
-          <li className="has-child flex items-center mx-3.5">
-            <span
-              className={`mr-[0.38rem] not-italic font-bold ${
-                styleNavbar ? 'text-[#454545]' : 'text-[#fff]'
-              } text-[1.125rem] leading-[1.575rem]`}
-            >
-              Giỏ hàng
-            </span>
-            <div className="relative">
-              <ICCart fill="#4DC0BD" width="1.47381rem" height="1.44581rem" />
-              <div className="flex items-center justify-center absolute -bottom-1.5 -right-1.5 bg-[#F58F5D] rounded-full w-[1.0625rem] h-[1.0625rem] font-bold not-italic text-[0.75rem]">
-                3
-              </div>
-            </div>
+          <li className="cursor-pointer has-child flex items-center mx-3.5">
+            <HoverCard openDelay={0}>
+              <HoverCardTrigger asChild>
+                <Link href="/cart" className="flex relative">
+                  <span
+                    className={`mr-[0.38rem] not-italic font-bold ${
+                      styleNavbar ? 'text-[#454545]' : 'text-[#fff]'
+                    } text-[1.125rem] leading-[1.575rem]`}
+                  >
+                    Giỏ hàng
+                  </span>
+                  <div className="relative">
+                    <ICCart
+                      fill={`${!styleNavbar ? '#fff' : '#4DC0BD'}`}
+                      width="1.47381rem"
+                      height="1.44581rem"
+                    />
+                    <div className="flex items-center justify-center absolute -bottom-1.5 -right-1.5 bg-[#F58F5D] rounded-full w-[1.0625rem] h-[1.0625rem] font-bold not-italic text-[0.75rem]">
+                      3
+                    </div>
+                  </div>
+                </Link>
+              </HoverCardTrigger>
+              <DropdownCartHeader />
+            </HoverCard>
           </li>
         </ul>
       </div>
