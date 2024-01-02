6aeaa903fcc08c4918652bba0c7d114d850be0cd
Tuấn Tùng Nguyễn <tuantung001@gmail.com>
2024-01-02 11:45:45 +0000

    Feat/api login

--- src/sections/home/view/CollectNew/Item.tsx
+++ src/sections/home/view/CollectNew/Item.tsx
@@ -97,7 +97,7 @@ function ItemCollect({ dataCollect, key }: IPropsItemCollect) {
           </div>
           <Link
             href={`/detail/${dataCollect?.id}`}
-            className="bg-white md:bg-[#55D5D2] price-product-slide flex justify-between items-center px-[3rem] md:px-[1.25rem] py-[0.5rem] rounded-[13.33333rem] md:rounded-[3.125rem]"
+            className="max-sm:border border-[#55D5D2] bg-white md:bg-[#55D5D2] price-product-slide flex justify-between items-center px-[3rem] md:px-[1.25rem] py-[0.5rem] rounded-[13.33333rem] md:rounded-[3.125rem]"
           >
             <p className="text-[4.8rem] md:text-[1.5rem] font-[850] text-[#55D5D2] md:text-[#fff]">
               {dataCollect?.price &&
